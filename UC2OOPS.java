7413bdc1ba041de32b5a9c795e8b93a699d244cd
jk1375-ship-it <jk1375@srmist.edu.in>
2026-02-16 18:07:47 +0530

    updated uc200==oops banner formatting

--- UC2OOPS.java
+++ UC2OOPS.java
@@ -1,11 +1,13 @@
-public class BannerConsole {
-	public static void main(String[] args) {
-		System.out.println(" **   **   **   ** ");
-		System.out.println("*     * *     * *     * *     *");
-		System.out.println("*     * *     * *     * *     *");
-		System.out.println("*     * *     * *     * *     *");
-		System.out.println("*     * *     * *     * *     *");
-		System.out.println("*     * *     * *     * *     *");
-		System.out.println(" **   **   **   ** ");
-	}
+public class UC2OOPS {
+    public static void main(String[] args) {
+
+        System.out.println("  *****  " + "  *****  " + "  ****** " + "  ****** ");
+        System.out.println(" *     * " + " *     * " + " *     * " + " *      ");
+        System.out.println("*       *" + "*       *" + " *     * " + " *      ");
+        System.out.println("*       *" + "*       *" + " ******  " + "  ***** ");
+        System.out.println("*       *" + "*       *" + " *       " + "       *");
+        System.out.println(" *     * " + " *     * " + " *       " + " *     *");
+        System.out.println("  *****  " + "  *****  " + " *       " + "  ***** ");
+
+    }
 }
